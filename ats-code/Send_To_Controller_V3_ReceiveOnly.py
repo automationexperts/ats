c85cb2172c81e77bfe343c61d5dd8ed6afbd8d70
Stephen Wong <sbwong@aeven.net>
2018-06-29 21:40:50 -0600

    Debugging
    Made some changes so that the serial bus was configured correctly. This included parity_none, as well as two stop bits, and a few other changes.

--- ats-code/Send_To_Controller_V3_ReceiveOnly.py
+++ ats-code/Send_To_Controller_V3_ReceiveOnly.py
@@ -128,6 +128,8 @@ def DriveByte(a): #formats start byte properly pg 52
     #if this returns nothing then there is an error
     if a >= 0 and a <= 63: 
         return a
+    elif a == 127:
+        return a
 
 def FunctionAndLength(length,code): #formats 2nd byte pg53
     l = (length-1) * 32 #pg53, 32 is placeholder moving l to proper position
@@ -136,6 +138,10 @@ def FunctionAndLength(length,code): #formats 2nd byte pg53
 
 def OutData(data): #returns a list containing properly formatted integers to transmit data
     # input data must be a large integer we want to transmit
+    '''
+    :param data: a large integer that we want to transmit
+    :return:
+    '''
     BinData = bin(data)[2:] #converts input integer to binary string and chops off 0b
     length = len(BinData)   #number of binary digits in data
     b = divmod(length,7)
@@ -182,7 +188,7 @@ def CheckSum(BnA, BnMinus1A, DatalistA): # see page 55
     dataSum = 0
     for i in range(0,numData): # Calculates sum of data integers
         dataSum = dataSum + DatalistA[i]
-    S = BnA + BnMinus1A +dataSum
+    S = BnA + BnMinus1A + dataSum
     output = 0x80 + (S%128)
     return output
 
@@ -192,7 +198,7 @@ def CheckSum(BnA, BnMinus1A, DatalistA): # see page 55
 
 # initialize the pi's serial port to controller parameters
 # page 50 of controller manual
-ser = serial.Serial("/dev/serial0", baudrate =  38400, timeout = 2, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE)  #this is the only serial port we use on the pi
+ser = serial.Serial("/dev/serial0", baudrate =  38400, timeout = 15, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_TWO)  #this is the only serial port we use on the pi
 #ser.baudrate = 38400
 #ser.timeout = 2
 #ser.PARITY_NONE
@@ -204,13 +210,13 @@ ser = serial.Serial("/dev/serial0", baudrate =  38400, timeout = 2, bytesize = s
 #set the varibles below to set the data that will be sent.
 driveID = radius_drive_id
 ToDo = Turn_ConstSpeed #packet function code see page 53. Function Code
-packet = 0x01 #data to be sent. Data can be max speed, gear number, etc.
+packet = 1000 #data to be sent. Data can be max speed, gear number, etc.
 OutLen = OutputSize(packet) #of data bits see page 54 for how to set
 
 OutArray = bytearray(OutLen+3) # data to send to controller
 Bn = DriveByte(driveID)
 BnMinus1 = FunctionAndLength(OutLen, ToDo)
-DataList = OutData(packet) 
+DataList = OutData(packet)
 B0 = CheckSum(Bn, BnMinus1, DataList)
 
 #assign variables to output byte array
@@ -222,10 +228,18 @@ for i in range(2,OutLen+2):
 OutArray[OutLen+2] = B0
 
 #OutArray is now ready to be passed out to servo controller
+print(OutData(packet))
+print("OutArray Type")
+print(type(OutArray))
+print("OutArray")
+print(OutArray)
 ser.write(OutArray)
 
+
+
 #read 100 characters and store it in str msg
-msg = ser.read(100)
+print("message read")
+msg = ser.read(1000)
 
 #print the string msg
 print(msg)
