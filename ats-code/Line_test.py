f9f20ce5cf69679ad9933afcf1658baef78673f9
bradhenrie <39839038+bradhenrie@users.noreply.github.com>
2018-08-17 14:50:02 -0600

    Code to test circle and linear plotting
    Refined according to correspondance with Tian.

--- ats-code/Line_test.py
+++ ats-code/Line_test.py
@@ -16,9 +16,17 @@ ID_Z = 7 # fake drive ID of z axis
 General_ID = 127 # general drive ID used to address all drives
 
 Make_LinearLine = 0x02  #function code for linear line
+Make_CircularArc = 0x04
 
 com.Send(ID_X, Make_LinearLine, 100)
 com.Send(ID_Y, Make_LinearLine, 100)
 com.Send(ID_Z, Make_LinearLine, 100) #z is fake
 com.Send(General_ID, Make_LinearLine, 5) #feedrate
 
+com.Send(ID_X, Make_CircularArc, -100)
+com.Send(ID_Y, Make_CircularArc, 0)
+com.Send(ID_Z, Make_CircularArc, 0)
+com.Send(ID_X, Make_CircularArc, 100)
+com.Send(ID_Y, Make_CircularArc, 0)
+com.Send(ID_Z, Make_CircularArc, 0)
+com.Send(General_ID, Make_CircularArc,4) #may also be a negative 2 byte data bit to do CCW directions.
